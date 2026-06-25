25f77dcfd1b7166e5428d258365de9d39c818524
Radhey2565 <radheymohan614@gmail.com>
2026-06-25 13:42:24 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ const codeLines: string[] = [
   "from sklearn.linear_model import LinearRegression",
   "model = LinearRegression()",
   "model.fit(X_train, y_train)",
-  "                                       ",
+  "                           ",
   "print('AI Model Trained Successfully!')",
   
 ];
